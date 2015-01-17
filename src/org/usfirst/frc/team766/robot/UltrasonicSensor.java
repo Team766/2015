d44e6d3420a7254dcffb9e3ee9e52d7aaa8af8fd
dolphonie <patrick.d.kao@gmail.com>
2015-01-16 17:50:03 -0800

    Finished ultrasonic sensor code
    Ultrasonic code now finally works.

--- src/org/usfirst/frc/team766/robot/UltrasonicSensor.java
+++ src/org/usfirst/frc/team766/robot/UltrasonicSensor.java
@@ -1,16 +1,10 @@
 package org.usfirst.frc.team766.robot;
 
-import java.io.ByteArrayInputStream;
-import java.io.DataInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
-
 import edu.wpi.first.wpilibj.SerialPort;
-import edu.wpi.first.wpilibj.Timer;
 
 public class UltrasonicSensor implements Runnable {
 
+	private static final boolean PRINT_DATA = false;
 	private static final double TIMEOUT = 10;
 	private static UltrasonicSensor us;
 
@@ -29,59 +23,53 @@ public class UltrasonicSensor implements Runnable {
 		return distance;
 	}
 
-	public synchronized void setValue(double d) {
+	private synchronized void setValue(double d) {
 		distance = d;
 	}
 
-	private byte[] getByte() {// only returns one byte
-		double oldTime = Timer.getFPGATimestamp();
-		while (true) {
-			if (port.getBytesReceived() > 0) {
-				return port.read(1);
-			} else {
-				if (Timer.getFPGATimestamp() - oldTime >= TIMEOUT) {
-					return new byte[] { Byte.MIN_VALUE };
-				}
-			}
-			try {
-				Thread.sleep(10);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
+	private void pr(String printData) {
+		if (PRINT_DATA)
+			System.out.println("Ultrasonic Sensor: " + printData);
 	}
 
-	private double readValue() {
-		String distanceLine = new String();
+	private boolean isValid(String distance) {
+		if (distance.length() != 5)
+			return false;
+		if (distance.charAt(0) != 'R')
+			return false;
+		for (int i = 1; i < distance.length(); i++) {
+			char c = distance.charAt(i);
+			if (!Character.isDigit(c))
+				return false;
+		}
+		return true;
+	}
+
+	private String readLine() {
+		StringBuffer distanceLine = new StringBuffer();
 		while (true) {
-			byte[] curByte = getByte();
-			try {
-				String curString = new String(curByte, "US-ASCII");
-				if (curString.equals("\n")) {
-					if (distanceLine.length() == 5) {
-						curString = curString.substring(curString.indexOf('R'));
-						return Double.parseDouble(curString);
-					}
-				}
-				distanceLine += curString;
-			} catch (UnsupportedEncodingException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			byte[] curByte = port.read(1);
+			char c = (char) (curByte[0] & 0xFF);
+			pr("Characer Read: " + new Character(c).toString());
+			if (c == '\r') {
+				return distanceLine.toString();
 			}
+			distanceLine.append(c);
 		}
 	}
 
 	// thread so robot doesn't hang
 	public void run() {
 		port = new SerialPort(9600, SerialPort.Port.kOnboard, 8,
-				SerialPort.Parity.kNone, SerialPort.StopBits.kOne);// not sure
-																	// about
-																	// port type
+				SerialPort.Parity.kNone, SerialPort.StopBits.kOne);
+		port.setTimeout(TIMEOUT);
 		while (true) {
-			setValue(readValue());
+			String s = readLine();
+			pr("Line Read");
+			if (isValid(s)) {
+				setValue(Double.parseDouble(s.substring(s.indexOf('R') + 1)));
+			}
 		}
-
 	}
 
 	private SerialPort port;
--- src/org/usfirst/frc/team766/test/SensorEmulator.java
+++
@@ -1,5 +0,0 @@
-package org.usfirst.frc.team766.test;
-
-public class SensorEmulator {
-
-}
