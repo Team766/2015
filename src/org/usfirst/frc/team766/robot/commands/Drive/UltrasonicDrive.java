04dc8264160c1d6d796cf0f82213075c271f0518
dolphonie <patrick.d.kao@gmail.com>
2015-02-07 14:41:56 -0800

    Added Slowdown to Ultrasonic Drive
    Prevented robot from crashing full speed into box.

--- src/org/usfirst/frc/team766/robot/commands/Drive/UltrasonicDrive.java
+++ src/org/usfirst/frc/team766/robot/commands/Drive/UltrasonicDrive.java
@@ -5,47 +5,52 @@ import org.usfirst.frc.team766.robot.UltrasonicSensor;
 import org.usfirst.frc.team766.robot.commands.CommandBase;
 
 /**
- * 	Uses a PID for the ultrasonic sensor to drive forward
- *	@author Blevenson
+ * Uses a PID for the ultrasonic sensor to drive forward
+ *
+ * @author AWire
+ * @author PKao
+ * @author Blevenson
  */
 public class UltrasonicDrive extends CommandBase {
-
+	private static final double SLOWING_OFFSET = .2;
+	
 	private double distanceFromBox;
 	private boolean beSafe;
 	private double currentDistance;
-	
-	public UltrasonicDrive()
-	{
+
+	public UltrasonicDrive() {
 		this.beSafe = true;
 		distanceFromBox = 0;
 	}
-	
-    public UltrasonicDrive(double distance, boolean beSafe) {
-    	distanceFromBox = distance;
-    	this.beSafe = beSafe;
-    }
 
-    protected void initialize() {
-    	Drive.resetEncoders();
-    	currentDistance = 0;
-    }
+	public UltrasonicDrive(double distance, boolean beSafe) {
+		distanceFromBox = distance;
+		this.beSafe = beSafe;
+	}
 
-    protected void execute() {
-    	//Drive forward using the PID here for the Ultrasonic sensor
-    	currentDistance = UltrasonicSensor.getInstance().getDistanceDouble();
-    }
+	protected void initialize() {
+		Drive.resetEncoders();
+		currentDistance = 0;
+	}
 
-    protected boolean isFinished() {
-        return (currentDistance <= distanceFromBox) || 
-        		(beSafe && (Drive.getAverageEncoderDistance() >= RobotValues.safteyDriveDistance));
-    }
+	protected void execute() {
+		// Drive forward using the PID here for the Ultrasonic sensor
+		currentDistance = UltrasonicSensor.getInstance().getDistanceDouble();
+		Drive.setPower(Math.min((currentDistance/1000)+ SLOWING_OFFSET,1));
+	}
 
-    protected void end() {
-    	Drive.setPower(0.0d);
-    }
-    
-    protected void interrupted() {
-    	System.out.println("0 Nose!!!  Someone interrupted the UltrasonicDrive Command...");
-    	end();
-    }
+	protected boolean isFinished() {
+		return (currentDistance <= distanceFromBox)
+				|| (beSafe && (Drive.getAverageEncoderDistance() >= RobotValues.safteyDriveDistance));
+	}
+
+	protected void end() {
+		Drive.setPower(0.0d);
+	}
+
+	protected void interrupted() {
+		System.out
+				.println("0 Nose!!!  Someone interrupted the UltrasonicDrive Command...");
+		end();
+	}
 }
