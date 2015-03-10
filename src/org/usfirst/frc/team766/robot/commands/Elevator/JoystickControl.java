ab3dd1ace30727988b186141ac376d4691eff5da
dolphonie <patrick.d.kao@gmail.com>
2015-03-09 18:11:45 -0700

    Brake Now Automatically Engages During Joystick Control

--- src/org/usfirst/frc/team766/robot/commands/Elevator/JoystickControl.java
+++ src/org/usfirst/frc/team766/robot/commands/Elevator/JoystickControl.java
@@ -18,6 +18,7 @@ public class JoystickControl extends CommandBase {
 
 	// Called just before this Command runs the first time
 	protected void initialize() {
+		Elevator.setBrake(false);
 	}
 
 	// Called repeatedly when this Command is scheduled to run
